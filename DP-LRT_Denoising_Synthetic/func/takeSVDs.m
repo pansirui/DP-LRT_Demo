82b2f19298a03aada88bd37c7ba1bbe9fdf35002
pansirui <siruipan@163.com>
2026-02-02 16:47:54 +0800

    Update takeSVDs.m

--- DP-LRT_Denoising_Synthetic/func/takeSVDs.m
+++ DP-LRT_Denoising_Synthetic/func/takeSVDs.m
@@ -1,10 +1,4 @@
 function [U, S, V] = takeSVDs(U,S,V,A,endI,runPar)
-% U is 400*400*40
-% S is 400*150*40
-% V is 150*150*40
-% A is 400*150*40
-% endI is 21
-% runPar is 0
 
 if ~exist('runPar','var')
     runPar = false;
@@ -14,9 +8,6 @@ if ~runPar || parpool('size') == 0
 
     for i=1:endI
         [U1,S1,V1]=svd(A(:,:,i));
-        % U1 is400*400
-        % S1 is 400*150
-        % V1 is 150*150
         U(:,:,i)=U1; S(:,:,i)=S1; V(:,:,i)=V1;
     end
 else
@@ -26,5 +17,6 @@ else
         U(:,:,i)=U1; S(:,:,i)=S1; V(:,:,i)=V1;
     end
 end
+
 
 end
